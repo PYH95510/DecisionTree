a2857e0f07f09328bd69f25f2601aa891b8883cb
Yohan <pyh95510@gmail.com>
2022-11-17 02:34:15 -0500

    append done to strcutured list

--- ese589/decisiontree/DataReader.py
+++ ese589/decisiontree/DataReader.py
@@ -3,7 +3,7 @@ file=open("/Users/yohanpark/git/Decision Tree/ESE589/data/adult.data")
 
 
 x = np.array([(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)],
-             dtype=[('age', np.int32), ('workclass', np.int32), ('education', np.int32),('educationNum',np.int32),('maritialStatus',np.int32),('occupation',np.int32),('relationship',np.int32),('race',np.int32),('sex',np.int32),('capital_gain',np.int32),('capital_loss',np.int32),('hoursPerWeek',np.int32),('nativeCountry',np.int32),('more50K',np.float64)])
+             dtype=[('age', np.int32), ('workclass', (np.str_, 10)), ('education', (np.str_, 10)),('educationNum',(np.str_, 10)),('maritialStatus',(np.str_, 10)),('occupation',(np.str_, 10)),('relationship',(np.str_, 10)),('race',(np.str_, 10)),('sex',(np.str_, 10)),('capital_gain',(np.str_, 10)),('capital_loss',np.int32),('hoursPerWeek',np.int32),('nativeCountry',(np.str_, 10)),('more50K',(np.str_, 10))])
 
 data = np.zeros(14, dtype={'names':('age', 'workclass', 'education','educationNum','maritialStatus','occupation','relationship','race','sex','capital_gain','capital_loss','hoursPerWeek','nativeCountry','more50K'),
                           'formats':('i4', 'i4', 'i4','i4','i4','i4','i4','i4','i4','i4','i4','i4','i4','f8')})
@@ -17,7 +17,15 @@ for line in lines:
     cols = line.split(',')
     if len(cols) == 0:
         continue
-    np.append(x,cols[0],cols[1],cols[3],cols[4],cols[5],cols[6],cols[7],cols[8],cols[9],cols[12],cols[13],cols[14],axis=0)
+    tmtarray = np.array([(cols[0], cols[1], cols[2], cols[3], cols[4], cols[5], cols[6], cols[7], cols[8], cols[9], cols[10], cols[11], cols[12], cols[13])],
+             dtype=[('age', np.int32), ('workclass','U20'), ('education', (np.str_, 10)),('educationNum',(np.str_, 10)),('maritialStatus',(np.str_, 10)),('occupation',(np.str_, 10)),('relationship',(np.str_, 10)),('race',(np.str_, 10)),('sex',(np.str_, 10)),('capital_gain',(np.str_, 10)),('capital_loss',np.int32),('hoursPerWeek',np.int32),('nativeCountry',(np.str_, 10)),('more50K',(np.str_, 10))])
+    x = np.append(x,tmtarray,axis=0)
+
+print(x)
+
+    
+
+    
 
 
 
