3591cc9da78ea617e4afa4f7fdfc341bfe87d927
Yohan <pyh95510@gmail.com>
2022-11-18 17:42:49 -0500

    delete

--- ese589/decisiontree/DataReader.py
+++ ese589/decisiontree/DataReader.py
@@ -5,11 +5,8 @@ file=open("/Users/yohanpark/git/Decision Tree/ESE589/data/adult.data")
 x = np.array([(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)],
              dtype=[('age', np.int32), ('workclass', (np.str_, 10)), ('education', (np.str_, 10)),('educationNum',(np.str_, 10)),('maritialStatus',(np.str_, 10)),('occupation',(np.str_, 10)),('relationship',(np.str_, 10)),('race',(np.str_, 10)),('sex',(np.str_, 10)),('capital_gain',(np.str_, 10)),('capital_loss',np.int32),('hoursPerWeek',np.int32),('nativeCountry',(np.str_, 10)),('more50K',(np.str_, 10))])
 
-data = np.zeros(14, dtype={'names':('age', 'workclass', 'education','educationNum','maritialStatus','occupation','relationship','race','sex','capital_gain','capital_loss','hoursPerWeek','nativeCountry','more50K'),
-                          'formats':('i4', 'i4', 'i4','i4','i4','i4','i4','i4','i4','i4','i4','i4','i4','f8')})
 
 
-print(data['age'])
 print(x['age'])
 
 lines = file.readlines()
